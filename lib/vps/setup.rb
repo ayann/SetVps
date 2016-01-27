0814f8c8f41640bafb7da0020ddda41dfbe04e2e
Armand Niampa <armand.niampa@gmail.com>
2016-01-27 14:51:12 +0100

    Implement project name.

--- lib/vps/setup.rb
+++ lib/vps/setup.rb
@@ -2,16 +2,21 @@ module Vps
   class << self
     def setup address, project
       # Initialize resource in instances project
-      self.resource address, project
+      resource address, project
 
       # Install default Script
-      self.default_script
+      session :root, root_command
 
       # Sleep waiting server restart
       sleep 10
 
       # Install configuration Script
-      self.configuration_script
+      begin
+        session :rails, rails_command
+      rescue Exception => exception
+        sleep 2
+        retry
+      end
 
       logger "Done!"
     end
@@ -23,30 +28,6 @@ module Vps
       @project = project
     end
 
-    def default_script
-      session :root, rails_command
-    end
-
-    self.def configuration_script
-      session :rails, rails_command
-    end
-
-    def rails_command
-      command = Vps::Script.rails_script["cmd"]
-      [12, 15].each do |i|
-        command[i] = command[i] % @project
-      end
-      command
-    end
-
-    def root_command
-      Vps::Script.root_script["cmd"]
-    end
-
-    def log_stdout stream
-      print data  if stream == :stdout
-    end
-
     def session user, commands
       Net::SSH.start(@address, user) do |session|
         commands.each do |cmd|
@@ -56,6 +37,20 @@ module Vps
           end
         end
       end
+    end
+
+    def root_command
+      Vps::Script.root_script["cmd"]
+    end
+
+    def rails_command
+      Vps::Script.rails_script["cmd"].map do |cmd|
+        cmd % ([@project] * 2)
+      end
+    end
+
+    def log_stdout stream
+      print data  if stream == :stdout
     end
 
     def logger msg
